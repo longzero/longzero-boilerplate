9d2c15635c0ffdd9f653cdd56e36f4ad1aecb06c
Long Nguyen <long@longzero.com>
2017-05-23 10:58:24 -0400

    Making it work with PHP

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var argv = require('yargs').argv;
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
-var connect = require('gulp-connect');
+var connect = require('gulp-connect-php');
 var gulpCssPreprocessor = require('gulp-css-preprocessor');
 var gulpif = require('gulp-if');
 var gutil = require('gulp-util');
@@ -11,18 +11,18 @@ var i18n = require('gulp-html-i18n');
 var inlineCss = require('gulp-inline-css');
 var minify = require('gulp-minifier');
 var pug = require('gulp-pug');
+var rename = require('gulp-rename');
 
 var isProduction = (argv.production === undefined) ? false : true;
 
 
-// EMAIL CSS 
+// EMAIL CSS
 gulp.task('emailCSS', function() {
   return gulp.src('src/email/**/*.styl')
     .pipe(gulpCssPreprocessor())
     .pipe(autoprefixer({
       browsers: ['last 3 versions']
     }))
-    .pipe(gulp.dest('.tmp/'))
     .pipe(header('/* Generated on: ' + new Date() + ' */\n'))
     .pipe(gulp.dest('src/email/'));
 });
@@ -68,7 +68,7 @@ gulp.task('watchEmail', function () {
   gulp.watch('src/email/images/**/*',['emailImages']);
 });
 
-// TASK EMAIL 
+// TASK EMAIL
 gulp.task('email', ['emailCSS','emailHTML','emailImages','watchEmail']);
 
 
@@ -80,49 +80,50 @@ gulp.task('email', ['emailCSS','emailHTML','emailImages','watchEmail']);
 // Copy images
 gulp.task('images', function() {
   return gulp.src('src/images/**/*')
-    .pipe(gulp.dest('development/images/'))
+    .pipe(gulp.dest('public/images/'))
     .pipe(gulpif(argv.production, gulp.dest('public/images/')));
 });
 
-// CSS 
+// CSS
 // Put all vendor CSS together
 gulp.task('css', function() {
   return gulp.src(['src/vendors/**/*.css'], {base: 'src'})
     .pipe(concat('vendors.css'))
-    .pipe(header('/* Generated on: ' + new Date() + ' */\n'))
-    .pipe(gulp.dest('development/'))
+    .pipe(gulp.dest('public/css/'))
     .pipe(gulpif(argv.production, minify({minify: true,minifyCSS: true})))
-    .pipe(gulpif(argv.production, gulp.dest('public/')));
+    .pipe(gulpif(argv.production, header('/* Generated on: ' + new Date() + ' */\n')))
+    .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+    .pipe(gulpif(argv.production, gulp.dest('public/css/')));
 });
-// CSS preprocessor 
+// CSS preprocessor
 gulp.task('stylus', function() {
-  return gulp.src('src/css/**/*', {base: 'src'})
+  return gulp.src('src/css/style.styl', {base: 'src'})
     .pipe(gulpCssPreprocessor())
     .pipe(autoprefixer({
       browsers: ['last 3 versions']
     }))
-    .pipe(header('/* Generated on: ' + new Date() + ' */\n'))
-    .pipe(gulp.dest('development/'))
+    .pipe(gulp.dest('public/'))
     .pipe(gulpif(argv.production, minify({minify: true,minifyCSS: true})))
+    .pipe(gulpif(argv.production, header('/* Generated on: ' + new Date() + ' */\n')))
+    .pipe(gulpif(argv.production, rename({suffix: '.min'})))
     .pipe(gulpif(argv.production, gulp.dest('public/')));
 });
 
-// HTML 
+// PUG
 // Compile pug files into HTML and split languages.
 // Languages are defined by creating appropriate folders in the folder languages.
-gulp.task('html', function() {
+gulp.task('pug', function() {
   return gulp.src('src/pages/**/*.pug')
     .pipe(pug({
       pretty: true
     }))
-    .pipe(i18n({
-      createLangDirs: true,
-      langDir: 'src/languages/',
-      trace: true
-    }))
-    .pipe(gulp.dest('.tmp/'))
-    .pipe(header('<!-- Generated on: ' + new Date() + ' -->\n'))
-    .pipe(gulp.dest('development/'))
+    // .pipe(i18n({
+    //   createLangDirs: true,
+    //   langDir: 'src/languages/',
+    //   trace: true
+    // }))
+    .pipe(gulp.dest('public/'))
+    .pipe(gulpif(argv.production, header('<!-- Generated on: ' + new Date() + ' -->\n')))
     .pipe(gulpif(argv.production, gulp.dest('public/')));
 });
 gulp.task('htmlRoot', function() {
@@ -130,29 +131,38 @@ gulp.task('htmlRoot', function() {
     .pipe(pug({
       pretty: true
     }))
-    .pipe(header('<!-- Generated on: ' + new Date() + ' -->\n'))
-    .pipe(gulp.dest('development/'))
+    .pipe(gulp.dest('public/'))
+    .pipe(gulpif(argv.production, header('<!-- Generated on: ' + new Date() + ' -->\n')))
     .pipe(gulpif(argv.production, gulp.dest('public/')));
 });
 
-// JAVASCRIPT 
-// Put JS files together 
-var jsFiles = ['src/vendors/jquery-2.1.4.min.js','src/vendors/parsley.min.js','src/vendors/**/*.js','src/js/**/*.js'];
+// PHP
+gulp.task('php', function() {
+  return gulp.src('src/pages/**/*.php')
+    .pipe(gulp.dest('public/'))
+    .pipe(gulpif(argv.production, header('<!-- Generated on: ' + new Date() + ' -->\n')))
+    .pipe(gulpif(argv.production, gulp.dest('public/')));
+});
+
+
+// JAVASCRIPT
+// Put JS files together
+var jsFiles = ['src/js/**/*.js'];
 gulp.task('js', function() {
   return gulp.src(jsFiles, {base: 'src'})
     .pipe(concat('main-generated.js'))
-    .pipe(header('/* Generated on: ' + new Date() + ' */\n'))
-    .pipe(gulp.dest('development/'))
+    .pipe(gulp.dest('public/js/'))
     .pipe(gulpif(argv.production, minify({minify: true,minifyJS: true})))
-    .pipe(gulpif(argv.production, gulp.dest('public/')));
+    .pipe(gulpif(argv.production, header('/* Generated on: ' + new Date() + ' */\n')))
+    .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+    .pipe(gulpif(argv.production, gulp.dest('public/js/')));
 });
 
-// LOCAL SERVER
-// Start a local server for developement
+
+// LOCAL PHP SERVER
 gulp.task('connect', function() {
   connect.server({
-    port: 8080,
-    root: 'development'
+    base: 'public'
   });
 });
 
@@ -161,8 +171,9 @@ gulp.task('connect', function() {
 gulp.task('watch', function () {
   gulp.watch('src/vendors/**/*.css',['css']);
   gulp.watch('src/css/**/*.styl',['stylus']);
-  gulp.watch('src/**/*.yaml',['html']);
-  gulp.watch('src/**/*.pug',['html']);
+  gulp.watch('src/**/*.yaml',['pug']);
+  gulp.watch('src/**/*.pug',['pug']);
+  gulp.watch('src/**/*.php',['php']);
   gulp.watch('src/*.pug',['htmlRoot']);
   gulp.watch('src/js/**/*.js',['js']);
   gulp.watch('src/vendors/**/*.js',['js']);
@@ -172,5 +183,5 @@ gulp.task('watch', function () {
 
 // TASKS
 // Bundled tasks
-gulp.task('default', ['css','stylus','html','htmlRoot','js','images']);
-gulp.task('dev', ['css','stylus','html','htmlRoot','js','images','watch','connect']);
+gulp.task('default', ['css','stylus','pug','php','htmlRoot','js','images','watch','connect']);
+gulp.task('build', ['css','stylus','pug','php','htmlRoot','js','images']);
---
+++ src/pages/includes-php/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <title>Titre méta</title>
+    <meta name="description" content="Description méta">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@longzero">
+    <meta property="og:title" content="Titre méta">
+    <meta property="og:description" content="Description OG">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://long.loves.design">
+    <meta property="og:image" content="http://">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link href="../css/vendors.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+  </head>
+  <body class="fr">
